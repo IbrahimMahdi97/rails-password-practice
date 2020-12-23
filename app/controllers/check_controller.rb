d869d728ccf6838a8f27f28b60ee00a0c89200dc
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2020-12-23 21:05:22 +0300

    create post/check logic

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,9 +1,20 @@
 class CheckController < ApplicationController
   def new
-    @credentials = Check.new
+    @checks = Check.new
   end
 
-  # def show
-  #   @credential = Check.find(params[:id])
+  def show
+    @checks = Check.find(params[:id])
+    if :email == "ralf"  && :password == "poop"
+      alertmessage == "username and password correct"
+    else 
+      alertmessage == "incorrect password and/or username"
+    end 
+    return alertmessage
+  end
+
+  # private 
+  # def check_params
+  #   params.require(:email).permit(:passowrd)
   # end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/passcheck' => 'authentication#new'
   get '/check/new' => 'check#new'
-  resources :check
+  post '/check/new' => 'check#show'
+  # resources :check, :path => 'check#new'
 
 end
